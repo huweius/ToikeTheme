263e13efa64d5d9c52ef0d1f2c8d98364f945546
Willy Hu <huwilly.sh@gmail.com>
2016-07-23 18:59:50 -0700

    Added Template File

---
+++ header.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Willy HU
+ * Date: 7/23/16
+ * Time: 6:55 PM
+ */
+
+?>
+
+<html>
+<head>
+    <title>Toike Theme</title>
+    <link rel="stylesheet"
+</head>
+</html>
